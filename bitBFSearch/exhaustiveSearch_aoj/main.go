9dd28cbc1f19968b9772efe1dbef1c030d58f7a0
iyoshiha <yoshi3co@gmail.com>
2022-09-26 03:19:40 +0900

    main.go : compileerrorinaoj

--- bitBFSearch/exhaustiveSearch_aoj/main.go
+++ bitBFSearch/exhaustiveSearch_aoj/main.go
@@ -18,25 +18,39 @@ func setInput() (int, []int){
 }
 
 
+func add(index int, ans []int) []int{
+
+	ans[index] = 1
+	return  ans
+}
 
 func main() {
 
 	n, A := setInput()
 	q, M := setInput()
+	var ans []int = make([]int, q)
+	var total int = 0
 
-	for pattern, k := 0,int(math.Pow(2,float64(n))); k < q; k++ {
-			Println(pattern)
-		for i := 0; i < pattern ; i++ {
-			for total, j := 0, 0; j < n; j++ {
-			Println(pattern>>j&1 == 1)
-				if pattern>>j&1 == 1 {
-					total+=A[j]
-				}
-				if total==M[k] {
-					Println("yes")
-					break
-				}
+	pattern:= int(math.Pow(2,float64(n)))
+	for i := 0; i < pattern ; i++ {
+		for j := 0; j < n; j++ {
+		// Println(i>>j&1 == 1)
+			if i>>j&1 == 1 {
+				total+=A[j]
 			}
+		}
+		for k := 0; k < q; k++ {
+			if total==M[k] {
+				ans = add(k, ans)
+			} else {
+
+			}
+		}
+		total = 0
+	}
+	for _, v := range ans {
+		if v == 1 { Println("yes")
+		}else {
 			Println("no")
 		}
 	}

487fee1b0a6790cf8f7f78e8148686665e0c6615
iyoshiha <yoshi3co@gmail.com>
2022-09-26 01:13:38 +0900

    main.go : matigai

--- bitBFSearch/exhaustiveSearch_aoj/main.go
+++ bitBFSearch/exhaustiveSearch_aoj/main.go
@@ -5,5 +5,17 @@ import(
 )
 
 func main() {
-	Println(3&1)
+	var n byte
+	var A []int
+	var q byte
+	var M []int
+	var input
+
+	Scan($n)
+
+	for i, v := range n {
+		Scan($input)
+		A = append(A, input)
+	}
+	Println(A)
 }

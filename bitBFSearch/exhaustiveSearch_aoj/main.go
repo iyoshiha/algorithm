d7fa1002a03c5421349f72e48cf39001e6c171f5
iyoshiha <yoshi3co@gmail.com>
2022-09-26 02:17:56 +0900

    main.go : operator

--- bitBFSearch/exhaustiveSearch_aoj/main.go
+++ bitBFSearch/exhaustiveSearch_aoj/main.go
@@ -2,6 +2,7 @@ package main
 
 import(
 	. "fmt"
+	"math"
 )
 
 func setInput() (int, []int){
@@ -24,5 +25,10 @@ func main() {
 	q, M := setInput()
 	Println(A, n, q, M)
 
-
+	pattern := math.Pow(2,n);
+	for i := 0; i < pattern ; i++ {
+		for j := 0; j < n; j++ {
+			pattern >> j & 1
+		}
+	}
 }

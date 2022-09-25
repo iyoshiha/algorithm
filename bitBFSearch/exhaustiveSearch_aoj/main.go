8f2b137f470785f0f6e8800427a5c1be4fc6485d
iyoshiha <yoshi3co@gmail.com>
2022-09-26 01:36:45 +0900

    main.go : funcVariable

--- bitBFSearch/exhaustiveSearch_aoj/main.go
+++ bitBFSearch/exhaustiveSearch_aoj/main.go
@@ -4,24 +4,30 @@ import(
 	. "fmt"
 )
 
-func main() {
-	var n int 
-	var A []int
-//	var q int
-//	var M []int
-	var input int
-
-	Scan(&n)
-
-	// for i, v := range n {
-	A = func (n int, A []int) []int{
+func an(n int, A []int) []int{
+		var input int
 		for i := 0; i < n ; i++{
 			Scan(&input)
 			A = append(A, input)
 		}
 		return A
-	}(n, A)
-		Println(A)
+}
+
+
+
+func main() {
+	var n int 
+	var A []int
+//	var q int
+//	var M []int
+
+	Scan(&n)
+
+	// for i, v := range n {
+	var bn func(int, []int)[]int = an
+	Println(an)
+	Println(bn)
+	Println(A)
 
 
 }

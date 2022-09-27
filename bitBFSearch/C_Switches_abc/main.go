a96149c1a174d2aeaeef6e47c470b263c4ab7e03
iyoshiha <yoshi3co@gmail.com>
2022-09-27 23:53:05 +0900

    main.go :

--- bitBFSearch/C_Switches_abc/main.go
+++ bitBFSearch/C_Switches_abc/main.go
@@ -21,6 +21,7 @@ func main() {
 		k = append(k,tmp)
 		for j:=0;k[i]<j;j++{
 			Scan(&tmp)
+			tmp--
 			s[i] = append(s[i], tmp)
 		}
 	}
@@ -29,27 +30,10 @@ func main() {
 		p = append(p, tmp)
 	}
 
+	for i:=0;i<1<<n;i++{
+		for j:=0;j<
+		1&i>>s[j][k]
 
+	}
 
-	pattern:= int(math.Pow(2,float64(n)))
-	for i := 0; i < pattern ; i++ {
-		for j := 0; j < n; j++ {
-		// Println(i>>j&1 == 1)
-			if i>>j&1 == 1 {
-				total+=A[j]
-			}
-		}
-		for k := 0; k < q; k++ {
-			if total==M[k] {
-				ans = add(k, ans)
-			}
-		}
-		total = 0
-	}
-	for _, v := range ans {
-		if v == 1 { Println("yes")
-		}else {
-			Println("no")
-		}
-	}
 }

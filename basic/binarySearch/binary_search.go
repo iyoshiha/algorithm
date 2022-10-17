e873c548c86325e2ec3c8506782bd515bcdea7f8
iyoshiha <yoshi3co@gmail.com>
2022-10-17 23:08:54 +0900

    binary search added

---
+++ basic/binarySearch/binary_search.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var n int
+	var S []int
+	var q int
+	var T []int
+	var C int
+	var mid int
+
+	var getInput = 
+	func(n *int, S *[]int) {
+		var tmp int
+		fmt.Scan(n)
+		*S = make([]int, 0)
+		for i := 0; i < *n; i++ {
+			fmt.Scan(&tmp)
+			*S = append(*S, tmp)
+		}
+	}
+	getInput(&n, &S)
+	getInput(&q, &T)
+
+
+	for _, t := range T{
+		for left, right := 0, n; left < right; {
+			mid = (left+right)/2
+			if t == S[mid]{
+				C++
+				break 
+			} else if S[mid] < t {
+				left = mid + 1
+
+			} else if t < S[mid] {
+				right = mid
+			}
+		}
+	}
+	fmt.Println(C)
+}

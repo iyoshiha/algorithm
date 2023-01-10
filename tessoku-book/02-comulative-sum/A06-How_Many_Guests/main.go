e4b180e74d1076a7725b7487bb060b6861313db4
iyoshiha <yoshi3co@gmail.com>
2023-01-11 05:54:22 +0900

    getInputDone

--- tessoku-book/02-comulative-sum/A06-How_Many_Guests/main.go
+++ tessoku-book/02-comulative-sum/A06-How_Many_Guests/main.go
@@ -1,7 +1,23 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
+	var n, q int
+	// A -> days
+	var days// , l, r  []int
+	fmt.Println("enter num: ")
+	getInput(&n)
+	getInput(&q)
+	fmt.Println("the number you entered is ", n)
+}
 
+func getInput(v *int) {
+	fmt.Scan(v)
+}
 
-
+func getInputToArray(v *[]int) {
+	fmt.Scan(v)
 }
